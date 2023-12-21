6f4852f2dc0232a69184a2249504d3fe697f1981
lousydropout <lousydropout@gmail.com>
2023-12-21 04:55:25 -0800

    start on Bets

--- contracts/src/betting/lib.rs
+++ contracts/src/betting/lib.rs
@@ -2,7 +2,9 @@
 
 #[ink::contract]
 mod betting {
+    use ink::prelude::{string::String, vec::Vec};
     use ink::storage::Mapping;
+    const NUM_DECIMALS: u32 = 12;
 
     #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
     #[cfg_attr(feature = "std", derive(::scale_info::TypeInfo))]
@@ -11,21 +13,73 @@ mod betting {
         NotFinalDecisionMaker,
     }
 
+    #[derive(Debug, Default, PartialEq, Eq, scale::Encode, scale::Decode)]
+    #[cfg_attr(feature = "std", derive(::scale_info::TypeInfo))]
+    pub struct Bet {
+        amount_wagered: Balance,
+        bettor_1: Option<AccountId>,
+        bettor_2: Option<AccountId>,
+        criteria_for_winning: Option<String>,
+        event_decided_by: u64,
+    }
+
     #[ink(storage)]
     pub struct Betting {
+        /// The amount bettor_1 pays the smart contract to create a bet
+        bet_creation_fee: Balance,
+        /// A set of registered reviewers
         reviewers: Mapping<AccountId, ()>,
+        /// How many registered reviewers there are
         number_of_reviewers: u32,
+        /// The accountId that has final say should a reviewer's decision be appealed
         final_decision_maker: AccountId,
+        /// Number of bets that have been made
+        latest_bet: u32,
+        /// A vector of bet information
+        bets: Vec<Bet>,
     }
 
     impl Betting {
         #[ink(constructor)]
-        pub fn new(final_decision_maker: AccountId) -> Self {
+        pub fn new(final_decision_maker: AccountId, bet_creation_fee: Balance) -> Self {
             Self {
+                bet_creation_fee,
                 reviewers: Mapping::default(),
                 number_of_reviewers: 0,
                 final_decision_maker,
+                latest_bet: 0,
+                bets: Vec::default(),
             }
+        }
+        // --------------------------------------------------------
+        // Bet-related functions
+        // --------------------------------------------------------
+        #[ink(message, payable)]
+        pub fn create_bet(
+            &mut self,
+            bettor_2: Option<AccountId>,
+            criteria_for_winning: Option<String>,
+            event_decided_by: u64,
+        ) -> Result<Option<u32>, ()> {
+            if self.env().transferred_value() < self.bet_creation_fee {
+                return Err(());
+            }
+            let amount_wagered = self.env().transferred_value() - self.bet_creation_fee;
+
+            let bet = Bet {
+                amount_wagered,
+                bettor_1: Some(self.env().caller()),
+                bettor_2,
+                criteria_for_winning,
+                event_decided_by,
+            };
+
+            // update latest bet number
+            let bet_number = self.latest_bet;
+            self.latest_bet = self.latest_bet + 1;
+
+            self.bets.push(bet);
+            Ok(Some(bet_number))
         }
 
         // --------------------------------------------------------
@@ -65,7 +119,7 @@ mod betting {
         fn register_alice() {
             let alice = default_accounts().alice;
             set_next_caller(alice);
-            let mut betting = Betting::new(alice);
+            let mut betting = Betting::new(alice, 10_u128.pow(NUM_DECIMALS));
 
             // not registered yet
             assert_eq!(
