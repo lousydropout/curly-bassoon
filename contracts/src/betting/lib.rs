7a5684d2352e20200f4ebbb2b3b4526d22449e49
lousydropout <lousydropout@gmail.com>
2023-12-21 11:58:18 -0800

    update betting functions

--- contracts/src/betting/lib.rs
+++ contracts/src/betting/lib.rs
@@ -2,18 +2,28 @@
 
 #[ink::contract]
 mod betting {
+    use chrono::{DateTime, Utc};
+
     use ink::prelude::{string::String, vec::Vec};
     use ink::storage::Mapping;
 
     #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
     #[cfg_attr(feature = "std", derive(::scale_info::TypeInfo))]
     pub enum Error {
-        // Caller is not a final decision maker
+        /// Caller is not a final decision maker
         NotFinalDecisionMaker,
+        /// The requested Bet does not exist
+        BetDoesNotExist,
+        /// Bettor can only be 1 or 2
+        BettorDoesNotExist,
+        /// The String is not in a parseable datetime format
+        NotDatetimeString,
+        /// Not enough was sent
+        IssufficientAmountOfTokensSent,
     }
 
     /// Different states that a bet can be in
-    #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, scale::Encode, scale::Decode)]
     #[cfg_attr(feature = "std", derive(::scale_info::TypeInfo))]
     pub enum BetState {
         Created,
@@ -26,7 +36,7 @@ mod betting {
     }
 
     /// Information regarding a particular bet
-    #[derive(Debug, Default, PartialEq, Eq, scale::Encode, scale::Decode)]
+    #[derive(Debug, PartialEq, Eq, scale::Encode, scale::Decode)]
     #[cfg_attr(feature = "std", derive(::scale_info::TypeInfo))]
     pub struct Bet {
         /// How much is wagered on the event's outcome
@@ -36,11 +46,11 @@ mod betting {
         /// Who is bettor 2?
         bettor_2: Option<AccountId>,
         /// How is the winner decided?
-        criteria_for_winning: Option<String>,
+        criteria_for_winning: String,
         /// When will the event conclude by (in unix timestamp, milliseconds)
-        event_decided_by: u128,
+        event_decided_by: String,
         ///
-        state: Option<BetState>,
+        state: BetState,
     }
 
     #[ink(storage)]
@@ -71,37 +81,126 @@ mod betting {
                 bets: Vec::default(),
             }
         }
+
+        // --------------------------------------------------------
+        // Helper functions
+        // --------------------------------------------------------
+
+        /// Get contract balance
+        #[ink(message)]
+        pub fn balance(&self) -> Result<Balance, ()> {
+            Ok(self.env().balance())
+        }
+
+        /// Get bet creation fee
+        #[ink(message)]
+        pub fn get_bet_creation_fee(&self) -> Result<Balance, ()> {
+            Ok(self.bet_creation_fee)
+        }
+
+        /// Convert datetime to milliseconds since Unix epoch
+        #[ink(message)]
+        pub fn convert_datetime_to_ms(&self, dt: String) -> Result<i64, Error> {
+            match dt.as_str().parse::<DateTime<Utc>>() {
+                Ok(y) => Ok(y.timestamp_millis()),
+                Err(_) => Err(Error::NotDatetimeString),
+            }
+        }
+
         // --------------------------------------------------------
         // Bet-related functions
         // --------------------------------------------------------
         #[ink(message, payable)]
         pub fn create_bet(
             &mut self,
+            amount_to_wager: Balance,
             bettor_2: Option<AccountId>,
-            criteria_for_winning: Option<String>,
-            event_decided_by: u128,
-        ) -> Result<Option<u32>, ()> {
-            if self.env().transferred_value() < self.bet_creation_fee {
-                return Err(());
+            criteria_for_winning: String,
+            event_decided_by: String,
+        ) -> Result<Option<u32>, Error> {
+            if self.env().transferred_value() < self.bet_creation_fee + amount_to_wager {
+                return Err(Error::IssufficientAmountOfTokensSent);
             }
-            // The amount wagered is calculated based on how much the user sent
-            let amount_wagered = self.env().transferred_value() - self.bet_creation_fee;
+            if event_decided_by.as_str().parse::<DateTime<Utc>>().is_err() {
+                return Err(Error::NotDatetimeString);
+            }
 
             let bet = Bet {
-                amount_wagered,
+                amount_wagered: amount_to_wager,
                 bettor_1: Some(self.env().caller()),
                 bettor_2,
                 criteria_for_winning,
                 event_decided_by,
-                state: Some(BetState::Created),
+                state: BetState::Created,
             };
-
             // update latest bet number
             let bet_number = self.latest_bet;
             self.latest_bet = self.latest_bet + 1;
 
             self.bets.push(bet);
             Ok(Some(bet_number))
+        }
+
+        /// Get amount wagered
+        #[ink(message)]
+        pub fn get_amount_wagered(&self, n: u32) -> Result<Balance, Error> {
+            match self.bets.get(n as usize) {
+                Some(x) => Ok(x.amount_wagered),
+                None => Err(Error::BetDoesNotExist),
+            }
+        }
+
+        /// Get datetime of when even finishes by
+        #[ink(message)]
+        pub fn get_event_decided_by(&self, n: u32) -> Result<String, Error> {
+            match self.bets.get(n as usize) {
+                Some(x) => Ok(x.event_decided_by.clone()),
+                None => Err(Error::BetDoesNotExist),
+            }
+        }
+
+        /// Get datetime of when even finishes by
+        #[ink(message)]
+        pub fn get_event_decided_by_as_ms(&self, n: u32) -> Result<i64, Error> {
+            match self.bets.get(n as usize) {
+                Some(x) => self.convert_datetime_to_ms(x.event_decided_by.clone()),
+                None => Err(Error::BetDoesNotExist),
+            }
+        }
+
+        /// Get bet state
+        #[ink(message)]
+        pub fn get_bet_state(&self, n: u32) -> Result<BetState, Error> {
+            match self.bets.get(n as usize) {
+                Some(x) => Ok(x.state),
+                None => Err(Error::BetDoesNotExist),
+            }
+        }
+
+        /// Get criteria for winning
+        #[ink(message)]
+        pub fn get_criteria_for_winning(&self, n: u32) -> Result<String, Error> {
+            match self.bets.get(n as usize) {
+                Some(x) => Ok(x.criteria_for_winning.clone()),
+                None => Err(Error::BetDoesNotExist),
+            }
+        }
+
+        /// Get bettor account id
+        #[ink(message)]
+        pub fn get_bettor_account_id(
+            &self,
+            n: u32,
+            bettor: u8,
+        ) -> Result<Option<AccountId>, Error> {
+            match self.bets.get(n as usize) {
+                Some(x) => match bettor {
+                    1 => Ok(x.bettor_1),
+                    2 => Ok(x.bettor_2),
+                    _ => Err(Error::BettorDoesNotExist),
+                },
+                None => Err(Error::BetDoesNotExist),
+            }
         }
 
         // --------------------------------------------------------
@@ -123,28 +222,11 @@ mod betting {
         pub fn is_final_decision_maker(&self) -> Result<bool, ()> {
             Ok(self.final_decision_maker == self.env().caller())
         }
-
-        /// Get balance
-        #[ink(message)]
-        pub fn balance(&self) -> Result<Balance, ()> {
-            Ok(self.env().balance())
-        }
     }
 
     #[cfg(test)]
     mod tests {
         use super::*;
-        use chrono::DateTime;
-
-        fn iso8601_to_timestamp_millis(iso8601_string: &str) -> Option<u128> {
-            match DateTime::parse_from_rfc3339(iso8601_string) {
-                Ok(parsed_time) => {
-                    let duration = parsed_time.timestamp_millis();
-                    Some(duration as u128)
-                }
-                Err(_) => None,
-            }
-        }
 
         fn default_accounts() -> ink::env::test::DefaultAccounts<ink::env::DefaultEnvironment> {
             ink::env::test::default_accounts::<Environment>()
@@ -158,7 +240,7 @@ mod betting {
         fn register_alice() {
             let alice = default_accounts().alice;
             set_next_caller(alice);
-            let mut betting = Betting::new(alice, 1_u128.pow(10));
+            let mut betting = Betting::new(alice, 1);
 
             // not registered yet
             assert_eq!(
@@ -196,7 +278,7 @@ mod betting {
         fn create_bet() {
             let alice = default_accounts().alice;
             let bob = default_accounts().bob;
-            let event_concludes_by = iso8601_to_timestamp_millis("2023-12-21T00:00:00Z").unwrap();
+            let event_concludes_by = String::from("2023-12-21T00:00:00Z");
 
             set_next_caller(alice);
 
@@ -204,28 +286,46 @@ mod betting {
             let mut betting = Betting::new(alice, fee);
             assert!(betting.balance().unwrap() == 1_000_000); // alice has 1 million to begin with
 
-            let criteria_for_winning: Option<String> =
-                Some("Red wins game against blue on December 21st, 2023.".into());
+            let criteria_for_winning: String =
+                "Red wins game against blue on December 21st, 2023.".into();
 
-            let amount_sent = 10 * fee;
+            let amount_to_wager = 100_000;
+            let amount_sent = amount_to_wager + 2 * fee;
             let bet_number = ink::env::pay_with_call!(
-                betting.create_bet(Some(bob), criteria_for_winning.clone(), event_concludes_by),
+                betting.create_bet(
+                    amount_to_wager,
+                    Some(bob),
+                    criteria_for_winning.clone(),
+                    event_concludes_by.clone()
+                ),
                 amount_sent
             )
             .unwrap()
             .unwrap();
             assert_eq!(bet_number, 0);
 
-            let bet = betting.bets.get(bet_number as usize).unwrap();
-            assert_eq!(bet.bettor_1.unwrap(), alice);
-            assert_eq!(bet.bettor_2.unwrap(), bob);
-            assert_eq!(bet.criteria_for_winning, criteria_for_winning);
-            assert_eq!(bet.event_decided_by, event_concludes_by);
-            assert_eq!(bet.state, Some(BetState::Created));
-            let amount_wagered = bet.amount_wagered;
+            // check using getter methods
             assert_eq!(
-                amount_wagered,
-                amount_sent - fee,
+                betting.get_bettor_account_id(bet_number, 1),
+                Ok(Some(alice))
+            );
+            assert_eq!(betting.get_bettor_account_id(bet_number, 2), Ok(Some(bob)));
+            assert_eq!(
+                betting.get_criteria_for_winning(bet_number),
+                Ok(criteria_for_winning)
+            );
+            assert_eq!(
+                betting.get_event_decided_by(bet_number),
+                Ok(event_concludes_by)
+            );
+            assert_eq!(
+                betting.get_event_decided_by_as_ms(bet_number),
+                Ok(1703116800000)
+            );
+            assert_eq!(betting.get_bet_state(bet_number), Ok(BetState::Created));
+            let amount_wagered = betting.get_amount_wagered(bet_number).unwrap();
+            assert_eq!(
+                amount_wagered, amount_to_wager,
                 "Acutal amount: {amount_wagered}"
             );
         }
